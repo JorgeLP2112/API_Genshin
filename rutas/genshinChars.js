56ad4890f8a22888c840343cd5036c4f188bd95d
Jorge Martínez <jorgealexmtz35@gmail.com>
2023-12-16 12:37:17 -0600

    Optimizar PATCH

--- rutas/genshinChars.js
+++ rutas/genshinChars.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const GenshinChar = require('../models/genshinChar')
+const _ = require('lodash')
 
 //Obtener Todos
 // ^ Genshin Listo
@@ -60,57 +61,32 @@ router.post('/', async (req, res) => {
 //Actualizar
 // ^ Genshin Listo
 router.patch('/:id', getGenshinChar, async (req, res) => {
-    if (req.body.charName != null && req.body.charName != "") {
-        res.genshinChar.charName = req.body.charName
-    }
-    if (req.body.quality != null && req.body.quality != "") {
-        res.genshinChar.quality = req.body.quality
-    }
-    if (req.body.element != null && req.body.element != "") {
-        res.genshinChar.element = req.body.element
-    }
-    if (req.body.stats.level != null && req.body.stats.level != "") {
-        res.genshinChar.stats.level = req.body.stats.level
-    }
-    if (req.body.stats.baseHp != null && req.body.stats.baseHp != "") {
-        res.genshinChar.stats.baseHp = req.body.stats.baseHp
-    }
-    if (req.body.stats.baseAtk != null && req.body.stats.baseAtk != "") {
-        res.genshinChar.stats.baseAtk = req.body.stats.baseAtk
-    }
-    if (req.body.stats.baseDef != null && req.body.stats.baseDef != "") {
-        res.genshinChar.stats.baseDef = req.body.stats.baseDef
-    }
-    if (req.body.stats.specialStat.stat != null && req.body.stats.specialStat.stat != "") {
-        res.genshinChar.stats.specialStat.stat = req.body.stats.specialStat.stat
-    }
-    if (req.body.stats.specialStat.baseStat != null && req.body.stats.specialStat.baseStat != "") {
-        res.genshinChar.stats.specialStat.baseStat = req.body.stats.specialStat.baseStat
-    }
-    if (req.body.talents != null && req.body.talents != "") {
-        res.genshinChar.talents = req.body.talents
-    }
-    if (req.body.constellations != null && req.body.constellations != "") {
-        res.genshinChar.constellations = req.body.constellations
-    }
-    if (req.body.weapon != null && req.body.weapon != "") {
-        res.genshinChar.weapon = req.body.weapon
-    }
-    if (req.body.region != null && req.body.region != "") {
-        res.genshinChar.region = req.body.region
-    }
-    if (req.body.gender != null && req.body.gender != "") {
-        res.genshinChar.gender = req.body.gender
-    }
-    if (req.body.releaseDate != null && req.body.releaseDate != "") {
-        res.genshinChar.releaseDate = req.body.releaseDate
-    }
-    if (req.body.version != null && req.body.version != "") {
-        res.genshinChar.version = req.body.version
-    }
-    if (req.body.icon != null && req.body.icon != "") {
-        res.genshinChar.icon = req.body.icon
-    }
+
+    const fieldsToUpdate = [
+        'charName',
+        'quality',
+        'element',
+        'stats.level',
+        'stats.baseHp',
+        'stats.baseAtk',
+        'stats.baseDef',
+        'stats.specialStat.stat',
+        'stats.specialStat.baseStat',
+        'talents',
+        'constellations',
+        'weapon',
+        'region',
+        'gender',
+        'releaseDate',
+        'version',
+        'icon'
+    ];
+
+    // Filtra las propiedades de req.body que no son nulas ni están vacías
+    const updatedFields = _.pickBy(req.body, (value) => value != null && value !== "");
+
+    // Actualiza res.genshinChar con las propiedades filtradas
+    _.merge(res.genshinChar, updatedFields);
 
     try {
         const updateGenshinChar = await res.genshinChar.save()
